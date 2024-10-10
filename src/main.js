0d9ff79f1aa111e6f433fd55ac793afbb2261913
Your Name <2638817077@qq.com>
2024-10-10 10:25:09 +0800

    ui-demo

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-plus';
+ 
+Vue.use(ElementUI);
 createApp(App).mount('#app')
